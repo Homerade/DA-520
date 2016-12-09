3243d76004924022496f581fc0b5aa81a609d374
Homerade <mizzsvizz@gmail.com>
2016-12-09 15:31:02 -0500

    12/9

--- Project3/crop_paste3_3.py
+++ Project3/crop_paste3_3.py
@@ -4,7 +4,7 @@ from PIL import ImageFilter
 from PIL import ImageEnhance
 from math import floor
 
-im = Image.open("im_pancake.jpg")
+im = Image.open("im_scotch.jpg")
 px = im.load()
 
 y1 = im.height/9
@@ -14,26 +14,28 @@ rInt = random.randint(5,20)
 
 
 
-# for x in range(0,im.width):
-#     for y in range(0,im.height):
-        
-#         R = px[ x, y ][0]
-#         G = px[ x, y ][1]
-#         B = px[ x, y ][2]
+for x in range(0,im.width):
+    for y in range(0,im.height):
+    	R = px[ x, y ][0]
+        G = px[ x, y ][1]
+        B = px[ x, y ][2]
 
-# 	# 	ColorsOptions = [
-# 	# 		['R=R/3', 'G=G-80', 'B=R'],	#green
-# 	# 		['R=R-80', 'G=R', 'B=B/3'],	#grey
-# 	# 		['R=B', 'G=G-80', 'B=B/3'],	#orange
-# 	# 		['R=R/3', 'G=-80', 'B=G'],	#winter
-# 	# 		['R=R/3', 'G=R', 'B=B-80'],	#deep blue
-# 	# 		['R=R-80', 'G=G/3', 'B=G'],	#red
-# 	# 	]
+        ColorsOptions = [
+			[R/3, G-80, R],	#green
+			[R-80, R, B/3],	#grey
+			[B, G-80, B/3],	#orange
+			[R/3, -80, G],	#winter
+			[R/3, R, B-80],	#deep blue
+			[R-80, G/3, G],	#red
+			]
 
-# 		# ColorShade = random.choice(ColorsOptions)	
-#
+	ColorShade = random.choice(ColorsOptions)
 
-#         #px[x,y] = ( ColorShade )
+	R = R/3
+	G = G-80
+	B = R
+
+	px[x,y] = (ColorShade)
 
 
 for y in range(0, im.height, im.height/9):  #im.height/rInt: 'rInt = random.randint(7,20)' random line thicknesses
